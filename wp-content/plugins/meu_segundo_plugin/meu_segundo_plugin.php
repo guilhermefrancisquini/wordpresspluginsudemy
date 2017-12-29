e1cc1b0077db93d80a9b5bcc1ae9d8f4b8881cef
Guilherme Francisquini <guilherme.francisquini@hotmail.com>
2017-12-29 19:40:50 -0200

    refatorando o segudo plugin e aplicando o padrão de projeto singleton

--- wp-content/plugins/meu_segundo_plugin/meu_segundo_plugin.php
+++ wp-content/plugins/meu_segundo_plugin/meu_segundo_plugin.php
@@ -12,23 +12,44 @@
 
  // Desativar a action welcome panel
 
- remove_action('welcome_panel', 'wp_welcome_panel');
-
- add_action('welcome_panel', 'my_welcome_panel');
- function my_welcome_panel()
+ class SegundoPlugin
  {
-    echo '
-    <div class="welcome-panel-content">
-        <h3> Seja Bem vindo ao Painel Administrativo</h3>
-        <p> Siga-nos nas redes sociais </p>
-        <div id="icons">
-            <a href="#" target="_blank">
-                <img src="http://wordpress.dev/wp-content/uploads/2017/12/1474968161-youtube-circle-color.png">
-            </a>
-            <a href="#" target="_blank">
-                <img src="http://wordpress.dev/wp-content/uploads/2017/12/1474968150-facebook-circle-color.png">
-            </a>
+    private static $instance;
+    
+    public static function getInstance()
+    {
+        if(self::$instance == NULL)
+        {
+            self::$instance = new self();
+        }
+    }
+
+    private function __construct()
+    {
+        //Desativa a action welcome_panel
+        remove_action('welcome_panel', 'wp_welcome_panel');
+
+        //Adiciona nova action
+        add_action('welcome_panel', array($this, 'welcome_panel'));
+    }
+
+    public function welcome_panel()
+    {
+        echo '
+        <div class="welcome-panel-content">
+            <h3> Seja Bem vindo ao Painel Administrativo</h3>
+            <p> Siga-nos nas redes sociais </p>
+            <div id="icons">
+                <a href="#" target="_blank">
+                    <img src="http://wordpress.dev/wp-content/uploads/2017/12/1474968161-youtube-circle-color.png">
+                </a>
+                <a href="#" target="_blank">
+                    <img src="http://wordpress.dev/wp-content/uploads/2017/12/1474968150-facebook-circle-color.png">
+                </a>
+            </div>
         </div>
-    </div>
-    ';
- }
+        ';
+    }
+}
+
+SegundoPlugin::getInstance();
